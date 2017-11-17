8a7dead4f43fd6491edbe015cb914bc3013a53ee
oguyon <oliv.guyon@gmail.com>
2017-11-17 10:28:19 -1000

    msync

--- info.c
+++ info.c
@@ -166,7 +166,7 @@ void __attribute__ ((constructor)) libinit_info()
 int init_info()
 {
     strcpy(data.module[data.NBmodule].name, __FILE__);
-    strcpy(data.module[data.NBmodule].info, "image information and statistics");
+    strcpy(data.module[data.NBmodule].info, "milk    - Image information and statistics");
     data.NBmodule++;
 
 
