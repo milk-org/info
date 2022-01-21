5f78b93f6b16429c2c7b16b81305325cf2f12133
Olivier Guyon <oliv.guyon@gmail.com>
2022-01-21 06:11:15 -1000

    remove extra print

--- imagemon.c
+++ imagemon.c
@@ -130,64 +130,64 @@ errno_t printstatus(imageID ID)
     int    semval;
     long   s;
 
-    printw("%s  ", data.image[ID].name);
+    TUI_printfw("%s  ", data.image[ID].name);
 
     datatype = data.image[ID].md[0].datatype;
 
     if (datatype == _DATATYPE_UINT8)
     {
-        printw("type:  UINT8             ");
+        TUI_printfw("type:  UINT8             ");
     }
     if (datatype == _DATATYPE_INT8)
     {
-        printw("type:  INT8              ");
+        TUI_printfw("type:  INT8              ");
     }
 
     if (datatype == _DATATYPE_UINT16)
     {
-        printw("type:  UINT16            ");
+        TUI_printfw("type:  UINT16            ");
     }
     if (datatype == _DATATYPE_INT16)
     {
-        printw("type:  INT16             ");
+        TUI_printfw("type:  INT16             ");
     }
 
     if (datatype == _DATATYPE_UINT32)
     {
-        printw("type:  UINT32            ");
+        TUI_printfw("type:  UINT32            ");
     }
     if (datatype == _DATATYPE_INT32)
     {
-        printw("type:  INT32             ");
+        TUI_printfw("type:  INT32             ");
     }
 
     if (datatype == _DATATYPE_UINT64)
     {
-        printw("type:  UINT64            ");
+        TUI_printfw("type:  UINT64            ");
     }
     if (datatype == _DATATYPE_INT64)
     {
-        printw("type:  INT64             ");
+        TUI_printfw("type:  INT64             ");
     }
 
     if (datatype == _DATATYPE_FLOAT)
     {
-        printw("type:  FLOAT              ");
+        TUI_printfw("type:  FLOAT              ");
     }
 
     if (datatype == _DATATYPE_DOUBLE)
     {
-        printw("type:  DOUBLE             ");
+        TUI_printfw("type:  DOUBLE             ");
     }
 
     if (datatype == _DATATYPE_COMPLEX_FLOAT)
     {
-        printw("type:  COMPLEX_FLOAT      ");
+        TUI_printfw("type:  COMPLEX_FLOAT      ");
     }
 
     if (datatype == _DATATYPE_COMPLEX_DOUBLE)
     {
-        printw("type:  COMPLEX_DOUBLE     ");
+        TUI_printfw("type:  COMPLEX_DOUBLE     ");
     }
 
     sprintf(str, "[ %6ld", (long) data.image[ID].md[0].size[0]);
@@ -231,7 +231,7 @@ errno_t printstatus(imageID ID)
 
     strcpy(str, str1);
 
-    printw("%-28s\n", str);
+    TUI_printfw("%-28s\n", str);
 
     clock_gettime(CLOCK_REALTIME, &tnow);
     tdiff = info_time_diff(tlast, tnow);
@@ -241,54 +241,54 @@ errno_t printstatus(imageID ID)
     frequ   = (data.image[ID].md[0].cnt0 - cntlast) / tdiffv;
     cntlast = data.image[ID].md[0].cnt0;
 
-    printw("[write %d] ", data.image[ID].md[0].write);
-    printw("[status %2d] ", data.image[ID].md[0].status);
-    printw("[cnt0 %8d] [%6.2f Hz] ", data.image[ID].md[0].cnt0, frequ);
-    printw("[cnt1 %8d]\n", data.image[ID].md[0].cnt1);
+    TUI_printfw("[write %d] ", data.image[ID].md[0].write);
+    TUI_printfw("[status %2d] ", data.image[ID].md[0].status);
+    TUI_printfw("[cnt0 %8d] [%6.2f Hz] ", data.image[ID].md[0].cnt0, frequ);
+    TUI_printfw("[cnt1 %8d]\n", data.image[ID].md[0].cnt1);
 
-    printw("[%3ld sems ", data.image[ID].md[0].sem);
+    TUI_printfw("[%3ld sems ", data.image[ID].md[0].sem);
     for (s = 0; s < data.image[ID].md[0].sem; s++)
     {
         sem_getvalue(data.image[ID].semptr[s], &semval);
-        printw(" %6d ", semval);
+        TUI_printfw(" %6d ", semval);
     }
-    printw("]\n");
+    TUI_printfw("]\n");
 
-    printw("[ WRITE   ", data.image[ID].md[0].sem);
+    TUI_printfw("[ WRITE   ", data.image[ID].md[0].sem);
     for (s = 0; s < data.image[ID].md[0].sem; s++)
     {
-        printw(" %6d ", data.image[ID].semWritePID[s]);
+        TUI_printfw(" %6d ", data.image[ID].semWritePID[s]);
     }
-    printw("]\n");
+    TUI_printfw("]\n");
 
-    printw("[ READ    ", data.image[ID].md[0].sem);
+    TUI_printfw("[ READ    ", data.image[ID].md[0].sem);
     for (s = 0; s < data.image[ID].md[0].sem; s++)
     {
-        printw(" %6d ", data.image[ID].semReadPID[s]);
+        TUI_printfw(" %6d ", data.image[ID].semReadPID[s]);
     }
-    printw("]\n");
+    TUI_printfw("]\n");
 
     sem_getvalue(data.image[ID].semlog, &semval);
-    printw(" [semlog % 3d] ", semval);
+    TUI_printfw(" [semlog % 3d] ", semval);
 
-    printw(" [circbuff %3d/%3d  %4ld]",
-           data.image[ID].md->CBindex,
-           data.image[ID].md->CBsize,
-           data.image[ID].md->CBcycle);
+    TUI_printfw(" [circbuff %3d/%3d  %4ld]",
+                data.image[ID].md->CBindex,
+                data.image[ID].md->CBsize,
+                data.image[ID].md->CBcycle);
 
-    printw("\n");
+    TUI_printfw("\n");
 
     average = arith_image_mean(data.image[ID].name);
     imtotal = arith_image_total(data.image[ID].name);
 
     if (datatype == _DATATYPE_FLOAT)
     {
-        printw("median %12g   ", arith_image_median(data.image[ID].name));
+        TUI_printfw("median %12g   ", arith_image_median(data.image[ID].name));
     }
 
-    printw("average %12g    total = %12g\n",
-           imtotal / data.image[ID].md[0].nelement,
-           imtotal);
+    TUI_printfw("average %12g    total = %12g\n",
+                imtotal / data.image[ID].md[0].nelement,
+                imtotal);
 
     vcnt = (long *) malloc(sizeof(long) * NBhistopt);
     if (vcnt == NULL)
@@ -610,10 +610,10 @@ errno_t printstatus(imageID ID)
     RMS   = sqrt(RMS / data.image[ID].md[0].nelement);
     RMS01 = 0.9 * RMS01 + 0.1 * RMS;
 
-    printw("RMS = %12.6g     ->  %12.6g\n", RMS, RMS01);
+    TUI_printfw("RMS = %12.6g     ->  %12.6g\n", RMS, RMS01);
 
     print_header(" PIXEL VALUES ", '-');
-    printw("min - max   :   %12.6e - %12.6e\n", minPV, maxPV);
+    TUI_printfw("min - max   :   %12.6e - %12.6e\n", minPV, maxPV);
 
     if (data.image[ID].md[0].nelement > 25)
     {
@@ -637,8 +637,9 @@ errno_t printstatus(imageID ID)
                     (minPV + 1.0 * (maxPV - minPV) * (h + 1) / NBhistopt),
                     vcnt[h]);
 
-            printw("%s",
-                   line1); //(minPV + 1.0*(maxPV-minPV)*h/NBhistopt), (minPV
+            TUI_printfw(
+                "%s",
+                line1); //(minPV + 1.0*(maxPV-minPV)*h/NBhistopt), (minPV
             //+ 1.0*(maxPV-minPV)*(h+1)/NBhistopt), vcnt[h]);
             attron(COLOR_PAIR(customcolor));
 
@@ -646,12 +647,12 @@ errno_t printstatus(imageID ID)
             i   = 0;
             while ((cnt < wcol - strlen(line1) - 1) && (i < vcnt[h]))
             {
-                printw(" ");
+                TUI_printfw(" ");
                 i += (long) (vcntmax / (wcol - strlen(line1))) + 1;
                 cnt++;
             }
             attroff(COLOR_PAIR(customcolor));
-            printw("\n");
+            TUI_printfw("\n");
         }
     }
     else
@@ -660,7 +661,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %f\n", ii, data.image[ID].array.F[ii]);
+                TUI_printfw("%3ld  %f\n", ii, data.image[ID].array.F[ii]);
             }
         }
 
@@ -668,7 +669,9 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %f\n", ii, (float) data.image[ID].array.D[ii]);
+                TUI_printfw("%3ld  %f\n",
+                            ii,
+                            (float) data.image[ID].array.D[ii]);
             }
         }
 
@@ -676,7 +679,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5u\n", ii, data.image[ID].array.UI8[ii]);
+                TUI_printfw("%3ld  %5u\n", ii, data.image[ID].array.UI8[ii]);
             }
         }
 
@@ -684,7 +687,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5u\n", ii, data.image[ID].array.UI16[ii]);
+                TUI_printfw("%3ld  %5u\n", ii, data.image[ID].array.UI16[ii]);
             }
         }
 
@@ -692,7 +695,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5lu\n", ii, data.image[ID].array.UI32[ii]);
+                TUI_printfw("%3ld  %5lu\n", ii, data.image[ID].array.UI32[ii]);
             }
         }
 
@@ -700,7 +703,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5lu\n", ii, data.image[ID].array.UI64[ii]);
+                TUI_printfw("%3ld  %5lu\n", ii, data.image[ID].array.UI64[ii]);
             }
         }
 
@@ -708,7 +711,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5d\n", ii, data.image[ID].array.SI8[ii]);
+                TUI_printfw("%3ld  %5d\n", ii, data.image[ID].array.SI8[ii]);
             }
         }
 
@@ -716,7 +719,7 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5d\n", ii, data.image[ID].array.SI16[ii]);
+                TUI_printfw("%3ld  %5d\n", ii, data.image[ID].array.SI16[ii]);
             }
         }
 
@@ -724,9 +727,9 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5ld\n",
-                       ii,
-                       (long) data.image[ID].array.SI32[ii]);
+                TUI_printfw("%3ld  %5ld\n",
+                            ii,
+                            (long) data.image[ID].array.SI32[ii]);
             }
         }
 
@@ -734,9 +737,9 @@ errno_t printstatus(imageID ID)
         {
             for (unsigned long ii = 0; ii < data.image[ID].md[0].nelement; ii++)
             {
-                printw("%3ld  %5ld\n",
-                       ii,
-                       (long) data.image[ID].array.SI64[ii]);
+                TUI_printfw("%3ld  %5ld\n",
+                            ii,
+                            (long) data.image[ID].array.SI64[ii]);
             }
         }
     }
@@ -885,11 +888,8 @@ errno_t info_image_monitor(const char *ID_name, double frequ)
         {
             erase();
 
-            TUI_printfw("============ SCREENS");
-            TUI_printfw("---");
-            TUI_newline();
-            TUI_printfw("---  %d --------", MonMode);
-            TUI_newline();
+
+            //TUI_newline();
 
             if (MonMode == 0)
             {
@@ -923,165 +923,7 @@ errno_t info_image_monitor(const char *ID_name, double frequ)
         }
     }
 
-
-
     endwin();
 
-
-    /*  Initialize ncurses  */
-    /* if (initscr() == NULL)
-     {
-         fprintf(stderr, "Error initialising ncurses.\n");
-         exit(EXIT_FAILURE);
-     }
-     getmaxyx(stdscr,
-              infoscreen_wrow,
-              infoscreen_wcol);
-    cbreak();
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);
-    curs_set(0);
-    noecho();
-    */
-
-    /*
-        NBpix = npix;
-        if (NBpix > infoscreen_wrow)
-        {
-            NBpix = infoscreen_wrow - 4;
-        }
-
-        start_color();
-        init_pair(1, COLOR_BLACK, COLOR_WHITE);
-        init_pair(2, COLOR_BLACK, COLOR_RED);
-        init_pair(3, COLOR_GREEN, COLOR_BLACK);
-        init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(5, COLOR_RED, COLOR_BLACK);
-        init_pair(6, COLOR_BLACK, COLOR_RED);
-
-        long NBtsamples = 1024;
-
-        cnt        = 0;
-        int loopOK = 1;
-        int freeze = 0;
-
-        int part   = 0;
-        int NBpart = 4;
-
-        while (loopOK == 1)
-        {
-            usleep((long) (1000000.0 / frequ));
-            int ch = getch();
-
-            if (freeze == 0)
-            {
-                attron(A_BOLD);
-                sprintf(monstring, "Mode %d   PRESS x TO STOP MONITOR", MonMode);
-                print_header(monstring, '-');
-                attroff(A_BOLD);
-            }
-
-            switch (ch)
-            {
-            case 'f':
-                if (freeze == 0)
-                {
-                    freeze = 1;
-                }
-                else
-                {
-                    freeze = 0;
-                }
-                break;
-
-            case 'x':
-                loopOK = 0;
-                break;
-
-            case 's':
-                MonMode = 0; // summary
-                break;
-
-            case '0':
-                MonMode = 1; // Sem timing
-                sem     = 0;
-                break;
-
-            case '1':
-                MonMode = 1; // Sem timing
-                sem     = 1;
-                break;
-
-            case '2':
-                MonMode = 1; // Sem timing
-                sem     = 2;
-                break;
-
-            case '+':
-                if (MonMode == 1)
-                {
-                    NBtsamples *= 2;
-                }
-                break;
-
-            case '-':
-                if (MonMode == 1)
-                {
-                    NBtsamples /= 2;
-                }
-                break;
-
-            case KEY_UP:
-                if (MonMode == 1)
-                {
-                    NBpart++;
-                    part = -1;
-                }
-                break;
-
-            case KEY_DOWN:
-                if (MonMode == 1)
-                {
-                    NBpart--;
-                    part = -1;
-                }
-                break;
-            }
-
-            if (freeze == 0)
-            {
-                if (MonMode == 0)
-                {
-                    clear();
-                    printstatus(ID);
-                }
-
-                if (MonMode == 1)
-                {
-                    clear();
-
-                    if (part > NBpart - 1)
-                    {
-                        part = 0;
-                    }
-                    info_image_streamtiming_stats(ID_name,
-                                                  sem,
-                                                  NBtsamples,
-                                                  0,
-                                                  1); // part, NBpart);
-                    part++;
-                    if (part > NBpart - 1)
-                    {
-                        part = 0;
-                    }
-                }
-
-                refresh();
-
-                cnt++;
-            }
-        }
-        endwin();
-    */
     return RETURN_SUCCESS;
 }
